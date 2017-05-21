ea790e3603bc77c46850035d0b3a917cc85967f5
Brandon Helms <bhelms85@gmail.com>
2017-05-21 22:37:59 +0000

    updated for the day

---
+++ app/kore/userUtils.py
@@ -0,0 +1,171 @@
+import hashlib
+import re
+import uuid
+
+class UserInformation(object):
+
+    def __init__(self):
+        self.company = ""
+        self.job_title = ""
+        self.country = ""
+        self.state = ""
+        self.email = ""
+        self.encrypted_password = None
+        self.username = None
+        self.profile_pic = None
+        self.first_name = ""
+        self.last_name = ""
+        self.is_admin = "false"
+        self.is_eng = "false"
+        self.is_dev = "false"
+        self.full_name = '%s %s' %(self.first_name, self.last_name)
+        self.uuid = ""
+
+    def runErrorChecker(self):
+        if not self.isPasswordEncrypted():
+            return False
+
+        self.username = setUsername(self.username)
+        self.state = setState(self.state)
+        self.country = setCountry(self.country)
+        self.email = setEmail(self.email)
+        self.first_name = setName(self.first_name)
+        self.last_name = setName(self.last_name)
+        self.full_name = setFullName(self.first_name, self.last_name)
+
+        for k in [self.is_admin, self.is_eng, self.is_dev]:
+            if k not in ["true", "false"]:
+                return False
+
+        if self.username is False or self.email is False or self.country is False:
+            return False
+        else:
+            return True
+
+    def isPasswordEncrypted(self):
+        if self.encrypted_password is None:
+            return False
+
+        if re.match("^[a-f0-9]{128}$", self.encrypted_password):
+            return True
+        else:
+            return False
+
+class NewUser(object):
+
+    def __init__(self, request_form):
+        self.username = setUsername(request_form['username'])
+        self.encrypted_password = setPassword(self.username, request_form['password'])
+        self.job_title = request_form['job']
+        self.state = setState(request_form['state'])
+        self.country = setCountry(request_form['country'])
+        self.department = request_form['department']
+        self.email = setEmail(request_form['email'])
+        self.first_name = setName(request_form['firstname'])
+        self.last_name = setName(request_form['lastname'])
+        self.full_name = setFullName(self.first_name, self.last_name)
+        self.is_admin = self.setPrivStatus(request_form, 'role_adm')
+        self.is_eng = self.setPrivStatus(request_form, 'role_eng')
+        self.is_dev = self.setPrivStatus(request_form, 'role_dev')
+
+    def setPrivStatus(self, d, k):
+        try:
+            d[k]
+        except KeyError:
+            return "false"
+        else:
+            return "true"
+
+
+def setUUID(username):
+    return str(uuid.uuid5(uuid.NAMESPACE_DNS, username))
+
+def setFullName(first_name, last_name):
+    first = setName(first_name)
+    last = setName(last_name)
+    if not first:
+        first = ""
+
+    if not last:
+        last = ""
+    return "%s %s" %(first, last)
+
+def setName(name):
+    #set to match rules in static/js/gsdk-bootstrap-wizard.js
+    if name is None:
+        return ""
+
+    name = name.strip()
+    if len(name) < 3:
+        return ""
+
+    if len(name) > 20:
+        return False
+
+    if not re.match("^[A-Za-z]{3,20}$", name):
+        return False
+
+    #passed all checks, now formatting it
+    first_letter = name[0].upper()
+    rest_of_name = name[1:].lower()
+    return first_letter + rest_of_name
+
+def setUsername(username):
+    #set to match rules in static/js/gsdk-bootstrap-wizard.js
+    if username is None:
+        return False
+
+    username = username.strip()
+    if len(username) < 6 or len(username) > 15:
+        return False
+    elif not re.match("^[A-Za-z0-9_]{6,15}$", username):
+        return False
+
+    #passed all the checks, now formatting it
+    return username.lower()
+
+def setPassword(seed, password):
+    #set to match rules in static/js/gsdk-bootstrap-wizard.js
+    if password is None:
+        return False
+    elif len(password) < 7 or len(password) > 20:
+        return False
+    elif not re.match("^[^'\"\\s]{1,}$", password):
+        return False
+
+    #passed all the checks, now formatting it
+    password = password.strip()
+    encrypted_password = hashlib.sha512(seed + password).hexdigest()
+    return encrypted_password
+
+def setState(state):
+    if state is None or state.strip() == "":
+        return ""
+    return state.strip().split(" ")[0]
+
+def setCountry(country):
+    if country is None:
+        return ""
+    return country.strip()
+
+def setEmail(email):
+    #set to match rules in static/js/gsdk-bootstrap-wizard.js
+    if email is None:
+        return ""
+
+    email = email.strip()
+    if len(email) > 254 or len(email) < 3:
+        return False
+
+    email_parts = email.split("@")
+    if len(email_parts) != 2:
+        return False
+
+    for part in email_parts:
+        if not re.match("^[A-Za-z0-9._-]{1,}$", part):
+            return False
+
+    #passed all the checks, now formatting it
+    return email.lower()
+
+
---
+++ app/routes.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python           
+import json                 
+import os                   
+import kore                 
+                            
+from flask import Flask, render_template, redirect, url_for, request, session  
+                       
+#########[ GLOBAL PARAMS ]#################                        
+DEBUG = True                
+SRVHOST = '0.0.0.0'         
+SRVPORT = 80                
+                            
+#########[ BASIC MODULES ]#################                        
+def loginCheck(**kwargs):
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+    else:
+        return render_template(session.get('current_url'), user=users[session.get('username')], **kwargs)
+
+def updatePage(current_title, current_url):
+    if current_url == session.get('current_url'):
+        return
+
+    if session.get('previous_url') is None:
+        session['previous_url'] = "index.html"
+        session['previous_title'] = "home"
+
+    session['previous_url'] = session.get('current_url')
+    session['previous_title'] = session.get('current_title')
+    session['current_url'] = current_url
+    session['current_title'] = current_title
+                       
+#########[ APP STARTUP ]###################                        
+app = Flask(__name__)       
+                            
+########[ WEB PAGES ]#####################                         
+@app.route('/')             
+@app.route('/home')         
+@app.route('/index')        
+def home():                        
+    updatePage("home", "index.html")                                
+    return loginCheck()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global users
+    error = { 'code'   : None,
+              'message': None
+            }
+
+    if request.method == 'POST':
+        try:
+            username = request.form['username'].lower()
+            password = request.form['password']
+        except KeyError:
+            error['code'] = 2 #User is bypassing the UX
+            error['message'] = "Malicious login attempt"
+        else:
+            if users.get(username) is None:
+                error['code'] = 3 #User does not exists
+                error['message'] = "Invalid Creds"
+            else:
+                error['message'] = kore.neo4j.userLogin(username, password, db)
+                if error['message'] and users[username]:
+                    try:
+                        users[username]['login_atttempt'] += 1
+                    except KeyError:
+                        users[username]['login_attempt'] = 1
+                        error['code'] = 4 #Failed login
+                    else:
+                        if users[username]['login_attempt'] > 2:
+                             error['code'] = 5 #Force reCaptcha
+                        else:
+                             error['code'] = 4 #Failed login
+
+        if error['code'] is None:
+            session['username'] = username
+            session['logged_in'] = True
+            session['login_attempt'] = 0
+            session['sidebar_collapse'] = False
+            return redirect(url_for('home'))
+        else:
+            print error
+            return render_template('login.html', error=error)
+    elif request.method == 'GET':
+        if session.get('logged_in'):
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/user-control-panel', methods=['GET', 'POST'])
+def userControlPanel():
+    updatePage('userControlPanel', 'user-control-panel.html')
+
+    #registration piece
+    if request.method == 'POST' and session.get('logged_in'):
+        status = kore.createNewUser(request.form, db)
+        if status == "ok":
+            return redirect(url_for(session['current_title']))
+
+    try:
+        if not users[session['username']]['is_admin']:
+            return redirect(url_for('error'))
+    except KeyError:
+        return loginCheck()
+
+    return loginCheck(ucp = kore.templateUserControlPanel.UserControlPanelPage(db))
+
+@app.route('/update-user-role', methods=['POST'])
+def updateUserRole():
+    status = True
+    if session['logged_in'] and users[session['username']]['is_admin']:
+        status = kore.templateUserControlPanel.updateUserRole(db, request.form['name'], request.form['property'], request.form['value'])
+    else:
+        print "something is going horrible"
+
+    if status:
+        print "status is good"
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    else:
+        print "statis is no good"
+        return json.dumps({'success':False}), 401, {'ContentType':'application/json'}
+
+@app.route('/user-profile')
+def userProfile():
+    updatePage("userProfile", "user-profile.html")
+    return loginCheck()
+
+@app.route('/asset-tracking')
+def assetTracking():
+    updatePage("assetTracking", "asset-tracking.html")
+    return loginCheck()
+
+@app.route('/asset-discovery')
+def assetDiscovery():
+    updatePage("assetDiscovery", "asset-discovery.html")
+    return loginCheck()
+
+###############[ ERROR HANDLING ]########################
+@app.route('/error')
+def error():
+    pass
+
+if __name__ == '__main__':
+    users = kore.neo4j.getAllUsers()
+    db = kore.neo4j.Initialize()
+    app.secret_key = os.urandom(24)
+    app.run(host=SRVHOST, port=SRVPORT, debug=DEBUG)
